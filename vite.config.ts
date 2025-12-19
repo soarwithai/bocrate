1e9852b4b16312ec0b01269a7da8b83708a2ad4c
soarwithai <xiahuaaaa@gmail.com>
2025-12-19 11:59:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     return {
-      base: 'https://soarwithai.github.io/bocrate/',
+      // 将原来的 GitHub URL 改为 '/'
+      base: '/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      // No runtime API keys required for this static rate viewer
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
